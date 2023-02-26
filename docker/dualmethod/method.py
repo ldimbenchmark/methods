e296cdfd0d41c75b696de1425bd0448eb75216f0
DanielHabenicht <daniel-habenicht@outlook.de>
2023-02-26 16:21:14 +0000

    add initial

---
+++ docker/dualmethod/method.py
@@ -0,0 +1,21 @@
+import os
+import logging
+from ldimbenchmark import (
+    FileBasedMethodRunner,
+)
+
+from ldimbenchmark.methods.dualmethod import DUALMethod
+
+# read log level from environment variable
+logLevel = os.getenv("LOG_LEVEL", "INFO")
+print(f"Log Level {logLevel}")
+numeric_level = getattr(logging, logLevel, None)
+if not isinstance(numeric_level, int):
+    raise ValueError("Invalid log level: %s" % logLevel)
+
+logging.basicConfig(level=numeric_level, handlers=[logging.StreamHandler()])
+logging.getLogger().setLevel(numeric_level)
+
+if __name__ == "__main__":
+    runner = FileBasedMethodRunner(DUALMethod())
+    runner.run()
