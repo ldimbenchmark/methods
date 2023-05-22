973b38e1c112ccba828c50b4517fea61823f4c59
DanielHabenicht <daniel-habenicht@outlook.de>
2023-05-22 12:38:52 +0000

    add test methods

---
+++ methods/test_complexity_linear/method.py
@@ -0,0 +1,21 @@
+import os
+import logging
+from ldimbenchmark import (
+    FileBasedMethodRunner,
+)
+
+from ldimbenchmark.methods.mnf import MNF
+
+# read log level from environment variable
+logLevel = os.getenv("LOG_LEVEL", "INFO")
+
+numeric_level = getattr(logging, logLevel, None)
+if not isinstance(numeric_level, int):
+    raise ValueError("Invalid log level: %s" % logLevel)
+
+logging.basicConfig(level=numeric_level, handlers=[logging.StreamHandler()])
+logging.getLogger().setLevel(numeric_level)
+
+if __name__ == "__main__":
+    runner = FileBasedMethodRunner(TestExponentialComplexityLeakageDetectionMethod())
+    runner.run()
